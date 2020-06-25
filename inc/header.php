1394fdcc45ec3ab07bdda8bac71614eb52841903
shubha-v <44811669+shubha-v@users.noreply.github.com>
2020-06-25 19:57:39 +0530

    Create header.php

---
+++ inc/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">	
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="shortcut icon" href="booksimg/icon.jpg" />
+        <title>BookStore</title>
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+		<style>
+			.nav.navbar-nav.navbar-right li a
+			{
+				color: white;
+			}
+		</style>
+	</head>
+	<body>
+		<nav class="navbar navbar-inverse navbar-fixed-top">
+			<div class="container">
+				<div class="navbar-header">
+					<li class="active"><a class="navbar-brand" href="index.php">Bookstore</a></li>
+				</div>
+				<div id="navbar" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="books.php">Books</i></a></li>
+						<li><a href="contact.php">Contact</i></a></li>
+						<li><a href="cart.php">My Cart</i></a></li>
+						<li><a href="admin.php">Admin</i></a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</body>
+</html>
